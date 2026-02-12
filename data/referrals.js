4ee429ba69038affdca3729c00d4aeddf44298b9
pettypychen <drpettychen@gmail.com>
2026-02-12 10:44:49 +0800

    Update referrals.js

--- data/referrals.js
+++ data/referrals.js
@@ -1,37 +1,67 @@
 window.REFERRALS = [
   {
+    "id": "breast-breast_lump",
+    "name": "Breast Surgery - Breast Lump",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Breast Surgery / General Surgery\nReason: Breast lump for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ F __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Breast Surgery/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic with a palpable breast lump.\n\nHx:\nDuration: __\nLocation: L/R breast, __ o'clock\nPain: __\nSkin changes: __\nNipple discharge: __\nFHx breast/ovarian cancer: __\nPrevious mammogram/ultrasound: __\nLMP: __\n\nO/E:\nLump: __ cm, firm/soft, mobile/fixed, at __ o'clock\nSkin tethering: __\nNipple retraction: __\nAxillary lymphadenopathy: __\n\nKindly arrange imaging and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "cardio-abnormal_ecg",
+    "name": "Cardiology - Abnormal ECG",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Cardiology\nReason: Abnormal ECG for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Cardiology Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal ECG.\n\nECG findings: __ (Date: __)\nSymptoms: Nil chest pain, nil palpitations, nil dyspnoea, nil syncope\nCardiac risk factors: DM/HPT/HLD/Smoking/FHx IHD\nPrevious cardiac workup: __\n\nO/E:\nVitals: BP __ mmHg, HR __ bpm, SpO2 __%\nHS: Dual, no murmur\nChest: Clear\n\nKindly assess and advise on further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "cardio-abnormal_tmx",
+    "name": "Cardiology - Abnormal TMX",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Cardiology\nReason: Abnormal treadmill test for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Cardiology Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal treadmill test.\n\nTMX findings: __ (Date: __)\nDuration: __ minutes, __ METs\nSymptoms during test: __\nSymptoms at rest: Nil chest pain, nil dyspnoea, nil palpitations\nCardiac risk factors: DM/HPT/HLD/Smoking/FHx IHD\nPrevious cardiac workup: __\n\nO/E:\nVitals: BP __ mmHg, HR __ bpm, SpO2 __%\nHS: Dual, no murmur\nChest: Clear\n\nKindly arrange further evaluation (coronary angiogram/CT coronary angiogram) and management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "cardio-palpitations",
+    "name": "Cardiology - Palpitations",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Cardiology\nReason: Palpitations for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Cardiology Team,\n\nThe above patient was seen at Parkway Shenton Clinic with palpitations.\n\nHx:\nDuration: __\nFrequency: __\nOnset: Sudden/Gradual\nRegular/Irregular: __\nAssociated symptoms: Nil chest pain, nil dyspnoea, nil syncope, nil presyncope\nTriggers: Caffeine/Exercise/Stress/NIL\nCardiac risk factors: DM/HPT/HLD/Smoking/FHx IHD/FHx sudden cardiac death\n\nO/E:\nVitals: BP __ mmHg, HR __ bpm (regular/irregular), SpO2 __%\nHS: Dual, no murmur\nChest: Clear\nThyroid: __\n\nECG: __ (Date: __)\n\nKindly assess and arrange further workup (Holter/echocardiogram) as appropriate.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "gs-fobt",
+    "name": "Colorectal Surgery - FOBT Abnormal",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Colorectal Surgery / General Surgery\nReason: Abnormal FOBT - referral for colonoscopy\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Colorectal Surgery/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal FOBT result.\n\nFOBT result: __ (Date: __)\nSymptoms: Nil PR bleeding, nil change in bowel habits, nil abdominal pain, nil weight loss\nPrevious colonoscopy: __\nFHx colorectal cancer: __\n\nO/E:\nAbdomen: Soft nontender, nil palpable mass\nDRE: __\n\nKindly arrange for colonoscopy and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "ent-ebv_igg",
+    "name": "ENT - EBV IgG Positive",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: ENT / Haematology\nReason: EBV IgG positive for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear ENT/Haematology Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have positive EBV serology on screening.\n\nEBV VCA IgG: Positive (Date: __)\nEBV VCA IgM: __\nEBV EA IgG: __\nEBV EBNA IgG: __\nSymptoms: Nil sore throat, nil neck lumps, nil epistaxis, nil nasal obstruction, nil unilateral hearing loss, nil weight loss, nil night sweats, nil fever\nFHx NPC/lymphoma: __\nEthnicity: __\n\nO/E:\nCervical lymphadenopathy: __\nOropharynx: __\nNasal examination: __\n\nKindly assess and advise on need for nasopharyngoscopy/imaging to exclude NPC or other EBV-related pathology.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "gs-elevated_cea",
+    "name": "Gastroenterology - Elevated CEA",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Gastroenterology / General Surgery\nReason: Elevated CEA for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Gastroenterology/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an elevated CEA on routine bloods.\n\nCEA: __ ng/mL (Date: __)\nSmoking: __\nSymptoms: Nil weight loss, nil change in bowel habits, nil abdominal pain, nil PR bleeding\nPrevious colonoscopy: __\nFHx GI malignancy: __\n\nO/E:\nAbdomen: Soft nontender, nil palpable mass\nDRE: __\n\nKindly arrange further workup (colonoscopy, CT abdomen/pelvis).\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
     "id": "generic",
     "name": "Generic Referral",
     "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: __\nReason: __\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Colleague,\n\nThe above patient was seen at Parkway Shenton Clinic with the following complaint:\n__\n\nO/E:\n__\n\nImpression: __\n\nKindly assist in further evaluation and management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
   },
   {
-    "id": "fobt",
-    "name": "FOBT Abnormal - Colonoscopy",
-    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Colorectal Surgery / General Surgery\nReason: Abnormal FOBT - referral for colonoscopy\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Colorectal Surgery/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal FOBT result.\n\nFOBT result: __ (Date: __)\nSymptoms: Nil PR bleeding, nil change in bowel habits, nil abdominal pain, nil weight loss\nPrevious colonoscopy: __\nFHx colorectal cancer: __\n\nO/E:\nAbdomen: Soft nontender, nil palpable mass\nDRE: __\n\nKindly arrange for colonoscopy and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+    "id": "og-abnormal_pap",
+    "name": "O&G - Abnormal PAP Smear",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Obstetrics & Gynaecology\nReason: Abnormal PAP smear for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ F __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear O&G Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal PAP smear.\n\nPAP result: __ (Date: __)\nHPV co-testing: __\nHPV vaccination: __\nPrevious PAP history: __\nLMP: __\nParity: G__P__\nSymptoms: Nil abnormal vaginal bleeding, nil post-coital bleeding, nil vaginal discharge\nSmoking: __\n\nO/E:\nSpeculum: Cervix __\nBimanual: __\n\nKindly arrange colposcopy and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "ortho-back_pain",
+    "name": "Orthopaedic Surgery - Back Pain",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Orthopaedic Surgery (Spine)\nReason: Back pain for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Orthopaedic Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic with back pain.\n\nHx:\nDuration: __\nLocation: Cervical/Thoracic/Lumbar/Sacral\nOnset: Traumatic/Atraumatic\nRadiation: Nil/L/R leg, dermatomal pattern __\nNeurological symptoms: Nil numbness, nil weakness, nil bowel/bladder dysfunction\nRed flags: Nil trauma, nil fever, nil weight loss, nil night pain, nil Hx malignancy\nAggravating factors: __\nRelieving factors: __\nPrevious imaging: __\nPhysiotherapy: __\n\nO/E:\nInspection: __\nTenderness: __\nRange of motion: __\nStraight leg raise: __\nNeuro: Power __, sensation __, reflexes __\nSpecial tests: __\n\nKindly assess and arrange further imaging/management as appropriate.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "ortho-fracture",
+    "name": "Orthopaedic Surgery - Fracture",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Orthopaedic Surgery\nReason: Fracture management\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Orthopaedic Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have a fracture on imaging.\n\nHx:\nMechanism of injury: __\nDate/time of injury: __\nLocation: __\nAble to weight bear: Yes/No\nNeurovascular symptoms: Nil numbness, nil weakness, nil colour change\n\nX-ray findings: __ (Date: __)\nFracture site: __\nDisplacement: __\nAngulation: __\nIntra-articular: Yes/No\n\nO/E:\nSwelling: __\nDeformity: __\nTenderness: __\nRange of motion: __\nNeurovascular: Distal pulses __, sensation __, motor function __\nSkin integrity: Intact/Open\n\nManagement so far:\nSplint/Backslab applied: __\nAnalgesia: __\n\nKindly assess and advise on definitive management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
+  },
+  {
+    "id": "ortho-knee_pain",
+    "name": "Orthopaedic Surgery - Knee Pain",
+    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Orthopaedic Surgery\nReason: Knee pain for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Orthopaedic Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic with knee pain.\n\nHx:\nDuration: __\nLaterality: L/R/Bilateral\nOnset: Traumatic/Atraumatic\nLocation of pain: Medial/Lateral/Anterior/Posterior/Diffuse\nMechanical symptoms: Nil locking, nil giving way, nil clicking\nSwelling: __\nAble to weight bear: Yes/No\nAggravating factors: Walking/Stairs/Squatting\nRelieving factors: __\nPrevious knee injuries: __\nPrevious imaging: __\nPhysiotherapy: __\n\nO/E:\nInspection: Swelling __, deformity __, muscle wasting __\nTenderness: Medial/Lateral joint line, patella, tibial tuberosity\nRange of motion: Flexion __°, extension __°\nEffusion: __\nLigaments: Anterior/Posterior drawer __, Lachman __, valgus/varus stress __\nMenisci: McMurray test __\nPatella: Apprehension test __\n\nKindly assess and arrange further imaging/management as appropriate.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
   },
   {
     "id": "physio",
     "name": "Physiotherapy",
     "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Physiotherapy\nReason: __\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Physiotherapy Team,\n\nThe above patient was seen at Parkway Shenton Clinic with:\n__\n\nO/E:\n__\n\nImpression: __\n\nKindly assess and initiate appropriate physiotherapy.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
-  },
-  {
-    "id": "breast_lump",
-    "name": "Breast Lump",
-    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Breast Surgery / General Surgery\nReason: Breast lump for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ F __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Breast Surgery/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic with a palpable breast lump.\n\nHx:\nDuration: __\nLocation: L/R breast, __ o'clock\nPain: __\nSkin changes: __\nNipple discharge: __\nFHx breast/ovarian cancer: __\nPrevious mammogram/ultrasound: __\nLMP: __\n\nO/E:\nLump: __ cm, firm/soft, mobile/fixed, at __ o'clock\nSkin tethering: __\nNipple retraction: __\nAxillary lymphadenopathy: __\n\nKindly arrange imaging and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
-  },
-  {
-    "id": "elevated_cea",
-    "name": "Elevated CEA",
-    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Gastroenterology / General Surgery\nReason: Elevated CEA for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear Gastroenterology/General Surgery Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an elevated CEA on routine bloods.\n\nCEA: __ ng/mL (Date: __)\nSmoking: __\nSymptoms: Nil weight loss, nil change in bowel habits, nil abdominal pain, nil PR bleeding\nPrevious colonoscopy: __\nFHx GI malignancy: __\n\nO/E:\nAbdomen: Soft nontender, nil palpable mass\nDRE: __\n\nKindly arrange further workup (colonoscopy, CT abdomen/pelvis).\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
-  },
-  {
-    "id": "ebv_igg",
-    "name": "EBV IgG Positive",
-    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: ENT / Haematology\nReason: EBV IgG positive for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ __ __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear ENT/Haematology Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have positive EBV serology on screening.\n\nEBV VCA IgG: Positive (Date: __)\nEBV VCA IgM: __\nEBV EA IgG: __\nEBV EBNA IgG: __\nSymptoms: Nil sore throat, nil neck lumps, nil epistaxis, nil nasal obstruction, nil unilateral hearing loss, nil weight loss, nil night sweats, nil fever\nFHx NPC/lymphoma: __\nEthnicity: __\n\nO/E:\nCervical lymphadenopathy: __\nOropharynx: __\nNasal examination: __\n\nKindly assess and advise on need for nasopharyngoscopy/imaging to exclude NPC or other EBV-related pathology.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
-  },
-  {
-    "id": "abnormal_pap",
-    "name": "Abnormal PAP Smear",
-    "content": "REFERRAL LETTER\n\nDate: __\nReferred Specialty: Obstetrics & Gynaecology\nReason: Abnormal PAP smear for evaluation\n\nName: __\nNRIC: __\nAge/Sex: __ F __\n\nAllergies: __\nPMHx: __\nMedications: __\n\n---\n\nDear O&G Team,\n\nThe above patient was seen at Parkway Shenton Clinic and found to have an abnormal PAP smear.\n\nPAP result: __ (Date: __)\nHPV co-testing: __\nHPV vaccination: __\nPrevious PAP history: __\nLMP: __\nParity: G__P__\nSymptoms: Nil abnormal vaginal bleeding, nil post-coital bleeding, nil vaginal discharge\nSmoking: __\n\nO/E:\nSpeculum: Cervix __\nBimanual: __\n\nKindly arrange colposcopy and further management.\n\nThank you,\n\nDr Pin Yu Chen M62343A\nParkway Shenton Clinic"
   }
 ];
